5a0d1454057d5f48ba2e0fb7ff06fbd34e2542fb
Teo Dell'Amico <teo@wanderable.com>
2014-12-23 18:54:36 -0800

    updated gemfile for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nh::Application.config.secret_key_base = 'c0d0f15c7a9b861da159b2af473550d0eda40962f87fcb8d6a86337c6df0db5c2002fe8cecff5653b632f48b77ed35f923314d8534d2c67a9d9eabbf38fc5d23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nh::Application.config.session_store :cookie_store, key: '_nh_session'
